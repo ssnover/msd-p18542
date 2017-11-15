8f52efc3780a20bb9dbe8165f808fe000e314466
ssnover95 <sws7379@rit.edu>
2017-11-15 12:18:42 -0500

    First commit of files involved.

---
+++ prototyping/ipc-sockets/client2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    file: client2.py
+    author: Shane Snover
+    purpose: A small script for spinning up an IPC client to demonstrate how
+             information can be communicated via Linux sockets in Python.
+"""
+
+
+def main():
+    """
+    The entry point of the application.
+    :return: None
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ prototyping/ipc-sockets/server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    file: server.py
+    author: Shane Snover
+    purpose: A small script to spin up an IPC server to demonstrate controlling
+             the transfer of data between Python applications via Unix sockets.
+"""
+
+
+def main():
+    """
+    Entry point of the application.
+    :return: None
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
