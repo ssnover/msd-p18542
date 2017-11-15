1101e68a9b8f2ad8817919a869722361beec7266
ssnover95 <sws7379@rit.edu>
2017-11-15 12:23:14 -0500

    Initial file commit.

---
+++ prototyping/pi-camera/take-picture.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    file: take-picture.py
+    author: Shane Snover
+    purpose: This is a small script to test the most basic function of the
+             Raspberry Pi camera API in Python; taking a picture and saving it
+             to the filesystem.
+"""
+
+
+def main():
+    """
+    The entry point of the application.
+    :return: None
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
