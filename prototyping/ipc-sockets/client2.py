6fb8fad197cd3fcdce166a93d336c09939ad8328
ssnover95 <sws7379@rit.edu>
2017-11-15 13:52:21 -0500

    Created a socket wrapper and wrote simple code for the two sockets.

--- prototyping/ipc-sockets/client2.py
+++ prototyping/ipc-sockets/client2.py
@@ -6,13 +6,30 @@
              information can be communicated via Linux sockets in Python.
 """
 
+import os
+from unix_socket_client import UnixSocketClient
+
+SOCKET_PATH = os.path.join('tmp', 'prototype-socket.sock')
+
 
 def main():
     """
     The entry point of the application.
     :return: None
     """
-    pass
+    my_ipc_client = UnixSocketClient(SOCKET_PATH)
+    my_ipc_client.connect()
+    print("Socket is connected to server.")
+    print("Press Ctrl+C to quit.")
+
+    while True:
+        try:
+            text_to_send = input("> ")
+            my_ipc_client.transmit(text_to_send)
+        except KeyboardInterrupt:
+            print("Closing IPC client.")
+            my_ipc_client.close()
+            break
 
 
 if __name__ == "__main__":
---
+++ prototyping/ipc-sockets/unix_socket_client.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    file: unix-socket-client.py
+    author: Shane Snover
+    purpose: A wrapper for the Python API to the Unix socket for clients.
+"""
+
+import socket
+
+
+class UnixSocketClient:
+    """
+    Object for interfacing with a Unix socket server.
+    """
+
+    def __init__(self, socket_path):
+        self.socket_path = socket_path
+        self.client = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+
+    def connect(self):
+        self.client.connect(self.socket_path)
+        return self
+
+    def transmit(self, str_to_transmit):
+        self.client.send(str_to_transmit.encode('utf-8'))
+        return
+
+    def close(self):
+        self.client.close()
+
+    def get_socket_path(self):
+        return self.socket_path
