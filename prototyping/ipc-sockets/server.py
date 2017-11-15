bae2f169c83da814579c0ef41262e64371c71d62
ssnover95 <sws7379@rit.edu>
2017-11-15 14:15:20 -0500

    Defined the server script.

--- prototyping/ipc-sockets/server.py
+++ prototyping/ipc-sockets/server.py
@@ -6,13 +6,27 @@
              the transfer of data between Python applications via Unix sockets.
 """
 
+from prototype_globals import SOCKET_PATH
+from unix_socket_server import UnixSocketServer
+
 
 def main():
     """
     Entry point of the application.
     :return: None
     """
-    pass
+    my_ipc_server = UnixSocketServer(SOCKET_PATH)
+    my_ipc_server.bind()
+    print("IPC Server is listening for incoming traffic.")
+
+    try:
+        while True:
+            sent_string = my_ipc_server.receive(100)
+            print(sent_string)
+    except KeyboardInterrupt:
+        print("Shutting down the IPC server.")
+        my_ipc_server.close()
+        my_ipc_server.cleanup_socket_file()
 
 
 if __name__ == "__main__":
