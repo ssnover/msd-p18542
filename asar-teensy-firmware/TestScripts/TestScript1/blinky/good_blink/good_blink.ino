3d00f1b74211b9983a8ee543ebdf99f258632847
Aaron Mallabar <amallabar13@gmail.com>
2018-02-18 22:08:02 -0500

    Good blink on interrupt script for teensy (Replaced the one that only worked for uno

---
+++ asar-teensy-firmware/TestScripts/TestScript1/blinky/good_blink/good_blink.ino
@@ -0,0 +1,40 @@
+
+
+#include "PITimer.h"
+
+const int LED = 13;
+const double sPeriod = .1; 
+double Time = 0;
+double othertime = 0;
+bool Flag = 0;
+
+void callback();
+
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Timer Begin");
+  //delay(1000);
+  pinMode(LED, OUTPUT);
+  PITimer1.period(sPeriod);     // initialize timer1
+  PITimer1.start(callback);           // attaches callback() as a timer overflow interrupt
+
+}
+
+void loop()
+{
+  while (Flag)
+  {
+    Flag = false;
+    Serial.print("timerone: "); Serial.println(Time);
+    
+  }
+}
+
+void callback()
+{
+  digitalWrite(LED, digitalRead(13) ^ 1);
+  Time = Time + sPeriod;
+  Flag = true;
+}
