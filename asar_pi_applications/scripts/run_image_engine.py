557c39dffcaadd4b15747788aa43625771a355f9
ssnover95 <sws7379@rit.edu>
2018-04-23 14:42:40 -0400

    asar_pi_applications/scripts/run_image_engine.py

---
+++ asar_pi_applications/scripts/run_image_engine.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    file: run_image_engine.py
+    purpose: Takes a picture with the PiCamera and runs the vision software
+             in order to generate a map which is printed to the user.
+"""
+
+from asar_vision.homography_automated import automated_homography
+from asar_camera_engine.asar_camera_engine import ASARCameraEngine
+
+
+def imaging_test():
+    """
+    Takes a picture with the PiCamera and runs the vision software
+    in order to generate a map which is printed to the user.
+    """
+
+    my_camera = ASARCameraEngine(1,
+                                 os.path.join(os.sep, 'home', 'pi', 'asar', 'database'),
+                                 os.path.join(os.sep, 'home', 'pi', 'asar', 'images'))
+    image_path = my_camera.take_picture()
+    terrain_path = automated_homography(image_path)
+    terrain_file = open(terrain_path, 'r')
+    print(terrain_file.read())
+    terrain_file.close()
+    return
+
+
+if __name__ == "__main__":
+    imaging_test()
