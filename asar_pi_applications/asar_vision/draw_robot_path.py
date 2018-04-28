751924c5ebf63bd7057eda24697dd8674ecbaca8
MarkBatorski <mxb9697@rit.edu>
2018-04-28 02:55:14 -0400

    robot path

--- asar_pi_applications/asar_vision/draw_robot_path.py
+++ asar_pi_applications/asar_vision/draw_robot_path.py
@@ -1,23 +1,20 @@
 import cv2
-import json
+#import json
 
 path = []
 pixel_path = []
-def draw_robot_path(image):
-    parameters = json.load(open('terrain.txt'))
-    path = []
-    path = parameters['path']
-    print(len(parameters['pixel']))
-    print(len(parameters['coordinate']))
-    for i in range(0, len(parameters['pixel'])):
-        temp_coordinate = parameters['pixel'][i][0]
-        parameters['pixel'][i][0] = parameters['pixel'][i][1]
-        parameters['pixel'][i][1] = temp_coordinate
+def draw_robot_path(image, coordinates):
+    #parameters = json.load(open('terrain.txt'))
+
+    for i in range(0, len(coordinates)):
+        temp_coordinate = coordinates[i][0]
+        coordinates[i][0] = coordinates[i][1]
+        coordinates[i][1] = temp_coordinate
 
     for j in range(0, len(path)):
-        for i in range(0, len(parameters['pixel'])):
-            if path[j][0] == parameters['coordinate'][i][0] and path[j][1] == parameters['coordinate'][i][1]:
-                pixel_path.append(tuple(parameters['pixel'][i]))
+        for i in range(0, len(coordinates)):
+            if path[j][0] == coordinates[i][0] and path[j][1] == coordinates[i][1]:
+                pixel_path.append(tuple(coordinates[i]))
     thickness = int(5)
     tuple(pixel_path)
 
@@ -25,6 +22,4 @@ def draw_robot_path(image):
         cv2.line(image, pixel_path[k], pixel_path[k+1], (0, 0, 255), thickness)
         cv2.circle(image, pixel_path[k], 7, (0, 0, 255), -1)
 
-    cv2.imshow('path', image)
-    cv2.waitKey(0)
     return image
