bd0cdb23cef130a0a1b5fcd1a7db3dcd6736239f
ssnover95 <sws7379@rit.edu>
2018-04-28 03:26:32 -0400

    It works!

--- asar_pi_applications/main.py
+++ asar_pi_applications/main.py
@@ -5,6 +5,7 @@
 """
 
 import json
+import os
 
 from asar_config import ASAR_GLOBALS
 from asar_web_server.asar_web_server.gui_constants import STATE
@@ -32,13 +33,15 @@ def main():
             pass
         elif my_state == STATE['RUNNING']:
             # get the most recent image
-            image = get_most_recent_image()
+            image = os.path.join(os.sep, 'home', 'pi', 'asar', 'images', 'prototyping-image.jpg') #get_most_recent_image()
             terrain_path = automated_homography(image)  # eventually stick this into db
-            current_danger_setting, _, _ = get_current_settings()
+            #current_danger_setting, _, _ = get_current_settings()
+            current_danger_setting = 0
             robot_directions, coordinates, start_node, end_node = runSearch(terrain_path, current_danger_setting)
             image = draw_robot_path(image, coordinates)
             # should save those directions to a file
-            my_comms_server.raw_write(robot_directions.decode('ISO-8859-1'))
+            #my_comms_server.raw_write(robot_directions.decode('ISO-8859-1'))
+            my_comms_server.raw_write(robot_directions)
             break
         elif my_state == STATE['PAUSED']:
             pass
